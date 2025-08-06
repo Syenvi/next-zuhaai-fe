ab7c11c8b087030da65ecc3e5da62716b0c65d96
skuukzky <ryujinkun34@gmail.com>
2025-08-06 23:44:29 +0700

    feat : add server image config

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
         port: "5000",
         pathname: "/product-images/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.zuhaai.ucappin.my.id",
+      },
     ],
   },
 };
